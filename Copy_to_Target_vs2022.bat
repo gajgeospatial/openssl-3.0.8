febe1e8cf4497887cd7bce5ab4557ad86e74e021
Glen A Johnson <glen.johnson@gaj-geospatial.com>
2026-01-31 15:20:12 -0500

    Add build option when environment not set

--- Copy_to_Target_vs2022.bat
+++ Copy_to_Target_vs2022.bat
@@ -1,3 +1,4 @@
+if exist ..\SetEnviornmentVariables.bat call ..\SetEnviornmentVariables.bat
 if exist %Dev_Base%\%GDAL_DIR%\msvc\vc143\x64\vs2022_Debug   copy /y vc143\x64\vs2022_Debug\libcrypto-3-x64.dll %Dev_Base%\%GDAL_DIR%\msvc\vc143\x64\vs2022_Debug
 if exist %Dev_Base%\%GDAL_DIR%\msvc\vc143\x64\vs2022_Debug   copy /y vc143\x64\vs2022_Debug\libcrypto-3-x64.pdb %Dev_Base%\%GDAL_DIR%\msvc\vc143\x64\vs2022_Debug
 if exist %Dev_Base%\%GDAL_DIR%\msvc\vc143\x64\vs2022_Debug   copy /y vc143\x64\vs2022_Debug\libssl-3-x64.dll %Dev_Base%\%GDAL_DIR%\msvc\vc143\x64\vs2022_Debug
